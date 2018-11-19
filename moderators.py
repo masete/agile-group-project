7a4be7d22f7c6b0bdc647721f8d05ce0291d5467
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-11-18 19:23:28 -0500

    Add feature to enable a moderator to delete [Delivers #162043917]

---
+++ moderators.py
@@ -0,0 +1,13 @@
+class Moderators():
+    """
+    class to define the functions performed by a moderator
+    """
+    # Function to delete comment by a moderator
+    def delete_comment_if_moderator(self, user_role, comment_id, commentList):
+       
+
+            
+
+
+
+
