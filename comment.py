13cb0dffde98f49260f91e84442be5702e5aab36
BagzieGracious <bagendadeogracious@gmail.com>
2018-11-19 11:30:56 +0300

    update comment file

--- comment.py
+++ comment.py
@@ -1,40 +1,37 @@
 import datetime
+
+
 class Comment:
 
-    def __init__(self):
-        self.commentList = []         
+    commentList = []
 
-    def add_comment(self, aurthor, message):
+    def add_comment(self, author, message):
         comment = {
-            'comment_id': len(self.commentList) + 1,
-            'aurthor': aurthor,
+            'comment_id': len(Comment.commentList) + 1,
+            'author': author,
             'message': message,
             'comment_date': datetime.datetime.now()
         }
         self.commentList.append(comment)
         return comment
 
-    def edit_comment(self, comment_id, aurthor, message):
-        for comment in self.commentList:
+    def edit_comment(self, comment_id, author, message):
+        for comment in Comment.commentList:
             if comment.__dict__['comment_id'] == comment_id:
-                if comment['aurthor'] == aurthor:
+                if comment['author'] == author:
                     comment['message'] = message
                     return comment
-                return 'only an aurthor can edit his/her comment'
+                return 'only an author can edit his/her comment'
         return 'comment not found'
 
     def delete_comment(self, comment_id, aurthor):
-        for comment in self.commentList:
+        for comment in Comment.commentList:
             if comment['comment_id'] == comment_id:
-                if comment['aurthor'] == aurthor:
-                    self.commentList.remove(comment)
+                if comment['author'] == aurthor:
+                    Comment.commentList.remove(comment)
                     return comment
                 return 'only an aurthor can delete his/her comment'
         return 'comment not found'
 
     def view_comment(self):
-        return self.commentList
- 
-
-if __name__ == '__main__':
-     Comment().add_comment('a', 'b')
+        return Comment.commentList
