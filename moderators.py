bd9c287977c3e3b6adb69f2c2c2febc5336d5d55
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-11-18 19:42:41 -0500

    integrate moderate with comment class

--- moderators.py
+++ moderators.py
@@ -1,11 +1,18 @@
-class Moderators():
+from comment import Comment
+class Moderators(Comment):
     """
     class to define the functions performed by a moderator
     """
-    # Function to delete comment by a moderator
-    def delete_comment_if_moderator(self, user_role, comment_id, commentList):
-       
 
+    def delete_comment_if_moderator(self,comment_id, author):
+        Comment().delete_comment(comment_id, author)
+
+
+moderator = Moderators()
+if __name__ == "__main__":
+    print(moderator.delete_comment_if_moderator(2, "Davis Keneth"))
+            
+                
             
 
 
